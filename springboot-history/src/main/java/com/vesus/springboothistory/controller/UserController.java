2726539e5bd5b6817992797287eef544490f95cc
vesus <liuwl@yundai365.com>
2018-06-26 16:48:19 +0800

    增加记录实体修改属性

--- springboot-history/src/main/java/com/vesus/springboothistory/controller/UserController.java
+++ springboot-history/src/main/java/com/vesus/springboothistory/controller/UserController.java
@@ -24,15 +24,19 @@ public class UserController {
     public String saveLog(){
        User user = new User() ;
        user.setId(1);
-       user.setName("李坤");
+       user.setName("张三");
 
         User user2 = new User() ;
         user2.setId(1);
         user2.setName("李四");
 
         BusinessChangeUtil<User> businessChangeUtil = new BusinessChangeUtil<User>();
-
+        //记录修改日志
         businessChangeUtil.record(User.class,user,user2,1,"vesus");
+        //记录增加日志
+        businessChangeUtil.record(User.class,null,user2,1,"vesus");
+        //记录删除日志
+        businessChangeUtil.record(User.class,user,null,1,"vesus");
 
        return "sucess" ;
     }
